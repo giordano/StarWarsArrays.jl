0ec2982bdc21109d4d2570c91a0b0ccde86dc55b
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-08-04 12:23:35 +0100

    Add code of the package

--- src/StarWarsArrays.jl
+++ src/StarWarsArrays.jl
@@ -1,5 +1,30 @@
 module StarWarsArrays
 
-greet() = print("Hello World!")
+export StarWarsArray
+
+struct StarWarsArray{T,N,P<:AbstractArray} <: AbstractArray{T,N}
+    parent::P
+end
+StarWarsArray(p::P) where {T,N,P<:AbstractArray{T,N}} =
+    StarWarsArray{T,N,P}(p)
+
+function index(i::Int)
+    if 4 <= i <= 6
+        return i - 3
+    elseif 1 <= i <= 3
+        return i + 3
+    else
+        return i
+    end
+end
+
+Base.parent(A::StarWarsArray) = A.parent
+Base.size(A::StarWarsArray) = size(parent(A))
+Base.getindex(A::StarWarsArray, i::Int) = getindex(parent(A), index(i))
+Base.getindex(A::StarWarsArray{T,N}, i::Vararg{Int,N}) where {T,N} =
+    getindex(parent(A), index.(i)...)
+Base.setindex!(A::StarWarsArray, v, i::Int) = setindex!(parent(A), v, index(i))
+Base.setindex!(A::StarWarsArray{T,N}, v, i::Vararg{Int,N}) where {T,N} =
+    setindex!(parent(A), v, index.(i)...)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using StarWarsArrays
 using Test
 
 @testset "StarWarsArrays.jl" begin
-    # Write your own tests here.
+    v = StarWarsArray(collect(1:9))
+    @test v[5] == 2
+    @test v[3] == 6
+    @test v[7] == 7
+    @test_throws BoundsError v[0]
+    v[4] = -42
+    @test v[4] == -42
+
+    m = StarWarsArray(reshape(collect(1:81), 9, 9))
+    @test m[5] == 2
+    @test m[3,4] == 6
+    @test m[7,2] == 43
+    @test m[4,4] == 1
+    @test_throws BoundsError m[0, 12]
+    m[4,4] = -42
+    @test m[4] == -42
 end
