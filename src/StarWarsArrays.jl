45877e891c6adcdcbe927da50166669d2590de0f
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-08-04 16:54:33 +0100

    Add Machete order

--- src/StarWarsArrays.jl
+++ src/StarWarsArrays.jl
@@ -1,14 +1,39 @@
 module StarWarsArrays
 
-export StarWarsArray
+export StarWarsArray, OriginalOrder, MacheteOrder
 
-struct StarWarsArray{T,N,P<:AbstractArray} <: AbstractArray{T,N}
+# Orders
+abstract type StarWarsOrder end
+struct OriginalOrder <: StarWarsOrder end
+struct MacheteOrder <: StarWarsOrder end
+
+# Exception
+struct StarWarsError <: Exception
+    i::Any
+    order::Any
+end
+
+function Base.showerror(io::IO, err::StarWarsError)
+    print(io, "StarWarsError: there is no episode $(err.i) in $(err.order)")
+end
+
+# The main struct
+struct StarWarsArray{T,N,P<:AbstractArray,O<:StarWarsOrder} <: AbstractArray{T,N}
     parent::P
 end
-StarWarsArray(p::P) where {T,N,P<:AbstractArray{T,N}} =
-    StarWarsArray{T,N,P}(p)
+function StarWarsArray(p::P, order::Type{<:StarWarsOrder}=OriginalOrder) where {T,N,P<:AbstractArray{T,N}}
+    StarWarsArray{T,N,P,order}(p)
+end
 
-function index(i::Int)
+machete_view_index(i) = range(1, stop=i)
+function StarWarsArray(p::P, order::Type{MacheteOrder}) where {T,N,P<:AbstractArray{T,N}}
+    StarWarsArray{T,N,P,order}(view(p, machete_view_index.(size(p) .- 1)...))
+end
+
+order(::StarWarsArray{T,N,P,O}) where {T,N,P,O} = O
+
+# Indexing
+function index(i::Int, ::Int, ::Type{OriginalOrder})
     if 4 <= i <= 6
         return i - 3
     elseif 1 <= i <= 3
@@ -17,14 +42,41 @@ function index(i::Int)
         return i
     end
 end
+function index(i::Int, size::Int, order::Type{MacheteOrder})
+    if 4 <= i <= 5
+        return i - 3
+    elseif 2 <= i <= 3
+        return i + 1
+    elseif  6 <= i <= size + 1
+        return i - 1
+    elseif i == 1
+        throw(StarWarsError(i,order))
+    else
+        return i
+    end
+end
 
+# Get the parent
 Base.parent(A::StarWarsArray) = A.parent
-Base.size(A::StarWarsArray) = size(parent(A))
-Base.getindex(A::StarWarsArray, i::Int) = getindex(parent(A), index(i))
+
+# Get the size
+Base.size(A::StarWarsArray{T,N,P,O}) where {T,N,P,O} = size(parent(A))
+
+# Get the elements
+Base.getindex(A::StarWarsArray, i::Int) =
+    getindex(parent(A), index(i, length(parent(A)), order(A)))
 Base.getindex(A::StarWarsArray{T,N}, i::Vararg{Int,N}) where {T,N} =
-    getindex(parent(A), index.(i)...)
-Base.setindex!(A::StarWarsArray, v, i::Int) = setindex!(parent(A), v, index(i))
+    getindex(parent(A), index.(i, size(parent(A)), order(A))...)
+Base.setindex!(A::StarWarsArray, v, i::Int) =
+    setindex!(parent(A), v, index(i, length(parent(A)), order(A)))
 Base.setindex!(A::StarWarsArray{T,N}, v, i::Vararg{Int,N}) where {T,N} =
-    setindex!(parent(A), v, index.(i)...)
+    setindex!(parent(A), v, index.(i, size(parent(A)), order(A))...)
+
+# Showing.  Note: this is awful, but it does what I want
+Base.show(io::IO, m::MIME"text/plain", A::StarWarsArray{T,N,P,MacheteOrder}) where {T,N,P} =
+    show(io, m,
+         view(parent(A),
+              map(i->StarWarsArrays.index.(i .+ 1, length(A), MacheteOrder),
+                  StarWarsArrays.machete_view_index.(size(A)))...))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
 using StarWarsArrays
 using Test
 
-@testset "StarWarsArrays.jl" begin
+import StarWarsArrays: StarWarsError, order
+
+@testset "OriginalOrder" begin
     v = StarWarsArray(collect(1:9))
+    @test order(v) == OriginalOrder
     @test size(v) == size(parent(v))
     @test v[5] == 2
     @test v[3] == 6
@@ -11,7 +14,8 @@ using Test
     v[4] = -42
     @test v[4] == -42
 
-    m = StarWarsArray(reshape(collect(1:81), 9, 9))
+    m = StarWarsArray(reshape(collect(1:81), 9, 9), OriginalOrder)
+    @test order(m) == OriginalOrder
     @test size(m) == size(parent(m))
     @test m[5] == 2
     @test m[3,4] == 6
@@ -22,4 +26,41 @@ using Test
     @test m[4] == -42
     m[1] = 0
     @test m[1,4] == 0
+end
+
+@testset "MacheteOrder" begin
+    v = StarWarsArray(collect(1:9), MacheteOrder)
+    @test order(v) == MacheteOrder
+    @test size(v) == (8,)
+    @test v[5] == 2
+    @test v[3] == 4
+    @test v[6] == 5
+    @test v[7] == 6
+    @test_throws StarWarsError v[1]
+    @test_throws BoundsError v[0]
+    v[4] = -42
+    @test v[4] == -42
+
+    m = StarWarsArray(reshape(collect(1:81), 9, 9), MacheteOrder)
+    @test order(m) == MacheteOrder
+    @test size(m) == (8, 8)
+    @test m[5] == 2
+    @test m[3,4] == 4
+    @test m[7,2] == 24
+    @test m[4,4] == 1
+    @test m[9,4] == 8
+    @test_throws BoundsError m[0, 12]
+    @test_throws BoundsError m[82]
+    @test_throws StarWarsError m[1]
+    @test_throws StarWarsError m[3, 1]
+    rpr = split(repr("text/plain", m), '\n')
+    @test rpr[2] == " 21  30  3  12  39  48  57  66"
+    @test rpr[6] == " 23  32  5  14  41  50  59  68"
+    m[4,4] = -42
+    @test m[4] == -42
+    m[2] = 0
+    @test m[2,4] == 0
+
+    @test sprint(showerror, StarWarsArrays.StarWarsError(1, MacheteOrder)) ==
+        "StarWarsError: there is no episode 1 in MacheteOrder"
 end
